d3b3e5715ba6e1cbb743f20095c03e7d600d797b
EloxFire <enzo.avagliano@ynov.com>
2020-06-22 21:09:54 +0200

    Added relations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 import Navbar from './components/sub/Navbar'
 import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar titles={"test", "test2"} />
-      <Home/>
-    </div>
+    <Router>
+      <Route path="/" exact component={props =>
+        <div>
+          <Navbar titles={["Home", "Work", "Blog", "Blinxon"]} />
+        </div>
+      }/>
+    </Router>
   );
 }
 
--- src/components/sub/Navbar.jsx
+++ src/components/sub/Navbar.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
+import {Link} from 'react-router-dom';
 
+import burgerMenuIcon from "../../img/burgerMenuIcon.svg";
 
 interface NavProps{
   titles: String[],
 }
-const Navbar:React.FC<NavProps> = ({titles}) => {
-  <div className="navbarBody col-12 d-flex flex-column align-items-center">
+const Navbar:React.FC<NavProps> = ({titles}) => (
+  <div className="col-12 d-flex flex-column align-items-center">
 
     <div className="col-12 d-none d-xs-none d-sm-none d-md-flex d-lg-flex flex-row align-items-center">
       <div className="col-5 d-flex justify-content-start">
         {titles.map(function(item,index){return <Link key={index} to={item.toLowerCase().replace(/\s/g, '')}><p key={index} className="m-2 navbarLink">{item}</p></Link>})}
       </div>
-      <div className="col-2 d-flex justify-content-center align-items-center">
+      {/*<div className="col-2 d-flex justify-content-center align-items-center">
         <img className="navbarLogo" src={logoEA} alt="Enzo Avagliano's logo"/>
-      </div>
+      </div>*/}
       {/*<div className="col-5 d-flex justify-content-end align-items-center">
         {rightTitles.map(function(item,index){return <Link key={index} to={item.toLowerCase().replace(/\s/g, '')}><p key={index} href="/" className="m-2 navbarLink">{item}</p></Link>})}
       </div>*/}
@@ -21,7 +23,7 @@ const Navbar:React.FC<NavProps> = ({titles}) => {
 
     <div className="col-12 d-flex d-xs-flex d-sm-flex d-md-none d-lg-none flex-row">
       <div className="dropdown col-12 ml-n4 d-flex align-items-center">
-        <img className="navbarLogo" src={logoEA} alt="Enzo Avagliano's logo"/>
+        {/*<img className="navbarLogo" src={logoEA} alt="Enzo Avagliano's logo"/>*/}
         <button className="btn dropdown-toggle d-flex align-items-center" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <img className="navbarLogo" src={burgerMenuIcon} alt="Burger menu icon"/>
         </button>
@@ -32,6 +34,6 @@ const Navbar:React.FC<NavProps> = ({titles}) => {
     </div>
 
   </div>
-}
+)
 
 export default Navbar;
